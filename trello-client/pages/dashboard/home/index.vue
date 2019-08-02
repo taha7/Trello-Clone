8fbd8337ba9b58eab26d8c028c55774e10d2e7a7
taha7 <taha.mohamed@iken.tech>
2019-08-03 00:41:12 +0200

    [front-end] make a different layout and use it inside a component

---
+++ trello-client/pages/dashboard/home/index.vue
@@ -0,0 +1,39 @@
+<template>
+   <div>
+      <!-- <Navbar /> -->
+      <section class="content-wrapper">
+         <div class="content-header">Hello Taha from home page</div>
+         <div class="content-body">Projects</div>
+      </section>
+   </div>
+</template>
+
+<script>
+// import Navbar from "../layouts/partials/navbar";
+export default {
+   layout: "master-container",
+   components: {
+      // Navbar
+   }
+};
+</script>
+
+<style scoped>
+.content-wrapper {
+   padding: 2rem 4rem;
+   width: 100%;
+   background: #e7eaf0;
+   min-height: calc(100vh - 56px);
+}
+
+.content-header {
+   background-color: red;
+   margin-bottom: 20px;
+}
+
+.content-body {
+   background-color: blueviolet;
+}
+</style>
+
+
--- trello-client/pages/index.vue
+++ trello-client/pages/index.vue
@@ -1,38 +1,5 @@
 <template>
-   <div>
-      <Navbar />
-      <section class="content-wrapper">
-         <div class="content-header">Hello Taha from home page</div>
-         <div class="content-body">Projects</div>
-      </section>
-   </div>
+   <div>hello welcome to our site</div>
 </template>
 
 <script>
-import Navbar from "../layouts/partials/navbar";
-export default {
-   components: {
-      Navbar
-   }
-};
-</script>
-
-<style scoped>
-.content-wrapper {
-   padding: 2rem 4rem;
-   width: 100%;
-   background: #e7eaf0;
-   min-height: calc(100vh - 56px);
-}
-
-.content-header {
-   background-color: red;
-   margin-bottom: 20px;
-}
-
-.content-body {
-   background-color: blueviolet;
-}
-</style>
-
-
