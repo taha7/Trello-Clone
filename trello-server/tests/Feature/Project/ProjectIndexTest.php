821a582c60b6e12b61cec0b4bf816ae8bdf9d208
taha7 <taha.mohamed@iken.tech>
2019-08-03 14:07:26 +0200

    Setup a testing enviroment

---
+++ trello-server/tests/Feature/Project/ProjectIndexTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\Project;
+
+use Tests\TestCase;
+
+class ProjectIndexTest extends TestCase
+{
+    /** @test */
+    public function it_lists_all_projects()
+    { }
+}
--- trello-server/tests/TestCase.php
+++ trello-server/tests/TestCase.php
@@ -3,8 +3,39 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestResponse;
+use App\Models\User;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->withoutExceptionHandling();
+
+        TestResponse::macro('jsonData', function ($key) {
+            return $this->decodeResponseJson()[$key];
+        });
+
+        // EloquentCollection::macro('assertEquals', function ($items) {
+        //     Assert::assertCount($items->count(), $this);
+
+        //     $this->zip($items)->each(function ($pair) {
+        //         Assert::assertTrue($pair[0]->is($pair[1]));
+        //     });
+
+        //     // dd(collect([1, 2, 3])->zip([4, 5, 6])); //[[1, 4], [2, 5], [3, 6]];
+        // });
+    }
+
+    protected function signIn($user = null)
+    {
+        $user = $user ?: create(User::class);
+        $this->actingAs($user);
+        return $this;
+    }
 }
