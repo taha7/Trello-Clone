67e363a82342c722aa0d25f36f10e374b044dac3
taha7 <taha.mohamed@iken.tech>
2019-08-10 19:57:44 +0200

    [Front-end] put navbar icons

--- trello-client/layouts/partials/navbar.vue
+++ trello-client/layouts/partials/navbar.vue
@@ -7,11 +7,22 @@
 
          <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav class="d-flex justify-content-center" style="flex: 1;">
-               <b-nav-item :to="{path: '/dashboard/home'}">Home</b-nav-item>
-               <b-nav-item :to="{path: '/projects'}">Projects</b-nav-item>
-               <b-nav-item href="#">Tasks</b-nav-item>
-               <b-nav-item href="#">Calendar</b-nav-item>
-               <b-nav-item href="#">Lists</b-nav-item>
+               <b-nav-item :to="{path: '/dashboard/home'}">
+                  <i class="nav-icons ion-ios-home-outline"></i>
+                  Home
+               </b-nav-item>
+               <b-nav-item :to="{path: '/projects'}">
+                  <i class="nav-icons ion-pull-request"></i>
+                  Projects
+               </b-nav-item>
+               <b-nav-item href="#">
+                  <i class="nav-icons ion-ios-list-outline"></i>
+                  Tasks
+               </b-nav-item>
+               <b-nav-item href="#">
+                  <i class="nav-icons ion-ios-calendar-outline"></i>
+                  Calendar
+               </b-nav-item>
             </b-navbar-nav>
 
             <!-- Right aligned nav items -->
@@ -33,49 +44,6 @@
          </b-collapse>
       </b-navbar>
    </div>
-   <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Task Fast</a>
-      <button
-         class="navbar-toggler"
-         type="button"
-         data-toggle="collapse"
-         data-target="#navbarSupportedContent"
-         aria-controls="navbarSupportedContent"
-         aria-expanded="false"
-         aria-label="Toggle navigation"
-      >
-         <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul class="navbar-nav d-flex justify-content-center" style="flex: 1;">
-            <li class="nav-item active">
-               <nuxt-link :to="{path: '/dashboard/home'}" class="nav-link">Home</nuxt-link>
-            </li>
-            <li class="nav-item">
-               <nuxt-link :to="{path: '/projects'}" class="nav-link">Projects</nuxt-link>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="#">Tasks</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="#">Callendar</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="#">Widgets</a>
-            </li>
-         </ul>
-         <form class="form-inline my-2 my-lg-0">
-            <input
-               class="form-control mr-sm-2"
-               type="search"
-               placeholder="Search"
-               aria-label="Search"
-            />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-         </form>
-      </div>
-   </nav>-->
 </template>
 
 <script>
@@ -83,4 +51,14 @@ export default {};
 </script>
 
 <style>
+.navbar {
+   padding: 0 1rem;
+}
+.nav-icons {
+   font-size: 25px;
+}
+.nav-item:hover a {
+   color: #2780f8 !important;
+   background: #e5e5e52e;
+}
 </style>
